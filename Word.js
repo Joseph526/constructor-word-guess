0c127aeff98b9344d3ed00fbc8579bd7e95506ac
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-04-22 19:39:42 -0400

    Adding methods to constructor

--- Word.js
+++ Word.js
@@ -1,12 +1,31 @@
 // Require external resources
 var Letter = require("./Letter.js");
 
+// Declare constructor function
+var Word = function(wordInput) {
+    
+    // Empty array to hold letter objects for each letter of wordInput
+    this.letterArr = [];
 
+    // Function to build array of letter objects
+    this.buildArr = function() {
+        for (var i = 0; i < wordInput.length; i++) {
+            var letterObj = new Letter(wordInput.split("")[i]);
+            this.letterArr.push(letterObj);
+        }
+    }
+
+    // Function to display string of letters or underscores
+    this.displayString = function() {
+        //
+    }
+}
+
+module.exports = Word;
 
 // Testing
-var testLetter = new Letter("a");
-console.log(testLetter);
-console.log(testLetter + "");
-testLetter.charEval("a");
-console.log(testLetter);
-console.log(testLetter + "");
+var testWord = new Word("peanut");
+console.log(testWord);
+console.log(testWord + " STRING TEST");
+testWord.buildArr();
+console.log(testWord.letterArr);
