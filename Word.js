ac9ddbe672630fc4bf3a245f69bc2ecce59c5716
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-04-22 18:37:15 -0400

    Building Word.js constructor

---
+++ Word.js
@@ -0,0 +1,12 @@
+// Require external resources
+var Letter = require("./Letter.js");
+
+
+
+// Testing
+var testLetter = new Letter("a");
+console.log(testLetter);
+console.log(testLetter + "");
+testLetter.charEval("a");
+console.log(testLetter);
+console.log(testLetter + "");
