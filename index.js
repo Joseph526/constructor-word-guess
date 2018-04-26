0e730845c17176af29a99b091b2495c9e748b797
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-04-25 23:49:05 -0400

    Adding dependencies

---
+++ index.js
@@ -0,0 +1,9 @@
+// Require external resources
+var Word = require("./Word.js");
+var prompt = require("prompt");
+
+// Start the prompt 
+prompt.start();
+
+// Declare global variables
+var gameBank = ["peanut", "sienna", "oafie"];
